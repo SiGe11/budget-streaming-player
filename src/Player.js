fd58dba2d1f910eb8d4aa826de0b360580db204c
SiGe11 <sim.gergely@gmail.com>
2022-12-06 11:55:23 +0100

    Use native hls js, remove wrapper package

--- src/Player.js
+++ src/Player.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import ReactHlsPlayer from 'react-hls-player';
-
+import HlsPlayer from './HlsPlayer';
 
 function Player() {
         return (
-            <ReactHlsPlayer
+            <HlsPlayer
             src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
             autoPlay={false}
             controls={true}
             width="100%"
-            height="auto"
-        />
+            height="auto"/>
     )
 }
 

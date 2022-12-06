1fd90fadadb1a8bde42665843e6f03900554b344
SiGe11 <sim.gergely@gmail.com>
2022-12-06 09:42:27 +0100

    Remove unneccesary files

---
+++ src/Player.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function Player() {
+    return (
+        <div className="Player">
+            player
+        </div>
+    );
+}
+
+export default Player;
